bbb6c39c5a4817ef052a3a5b19d690ed07f860da
w1024ji <w1024ji@gmail.com>
2025-12-28 17:25:29 +0900

    Create search_in_rotated_sorted_array.py

---
+++ leetcode/search_in_rotated_sorted_array.py
@@ -0,0 +1,32 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+
+        pivot = left      # pivot is the index of the smallest number
+
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            mid_pivot = (mid + pivot) % len(nums)    # pivot 만큼 밀려있음을 알려주기
+
+            if nums[mid_pivot] < target:
+                left = mid + 1
+            elif nums[mid_pivot] > target:
+                right = mid - 1
+            else:
+                return mid_pivot
+                
+        return -1
+
+        
