30dd9cb57e9fc2863517fa4a82a67f2575302c0c
w1024ji <w1024ji@gmail.com>
2025-12-30 10:49:56 +0900

    Create best_time_to_buy_and_sell_stock_2.py

---
+++ leetcode/best_time_to_buy_and_sell_stock_2.py
@@ -0,0 +1,8 @@
+def maxProfit(self, prices: List[int]) -> int:
+        result = 0
+        for i in range(len(prices) - 1):
+            if prices[i+1] > prices[i]:
+                result += prices[i+1] - prices[i]
+
+        return result
+        
