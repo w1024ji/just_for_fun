8859414d5bab2b38d661c366429003eac0e2fb7b
w1024ji <w1024ji@gmail.com>
2025-12-31 17:37:06 +0900

    Create fibonacci.py

---
+++ leetcode/dynamic/fibonacci.py
@@ -0,0 +1,27 @@
+# memoziation 이미 계산한 값은 저장해뒀다가 바로 리턴해주자!
+class Solution:
+  dp = collections.defaultdict(int)
+
+  def fib(self, N: int) -> int:
+    if N <= 1:
+      return N
+
+    if self.dp[N]:
+      return self.dp[N]
+
+    self.dp[N] = self.fib(N - 1) + self.fib(N - 2)
+    return self.dp[N]
+
+
+# tabulation
+class Solution:
+  dp = collections.defaultdict(int)
+
+  def fib(self, N: int) -> int:
+    self.dp[0] = 0
+    self.dp[1] = 1
+
+    for i in range(2, N+1):
+      self.dp[i] = self.dp[i - 1] + self.dp[i - 2]
+    return self.dp[N]
+
